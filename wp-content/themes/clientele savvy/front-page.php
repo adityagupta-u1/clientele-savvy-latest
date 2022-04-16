57a5ad2039ffd1eb6fd8c6f35d287139d4df462b
adityagupta-u1 <beenagupta934@gmail.com>
2022-04-16 20:01:10 +0530

    changed front-page.php

--- wp-content/themes/clientele savvy/front-page.php
+++ wp-content/themes/clientele savvy/front-page.php
@@ -39,6 +39,8 @@
 
         $mail->send();
         echo 'Message has been sent';
+
+        header('location:http://clientelesavvy.com/');
     } catch (Exception $e) {
         echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
     }
