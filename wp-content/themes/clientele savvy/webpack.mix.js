3dc7efe427fd477a17ed3679413778033710c3fd
adityagupta-u1 <beenagupta934@gmail.com>
2022-06-17 02:09:16 +0530

    done

--- wp-content/themes/clientele savvy/webpack.mix.js
+++ wp-content/themes/clientele savvy/webpack.mix.js
@@ -1,5 +1,6 @@
 let mix = require('laravel-mix');
 require('laravel-mix-polyfill');
+require('laravel-mix-purgecss');
 
 mix.js('src/js/app.js', 'dist/js')
    .sass('src/css/style.scss', 'dist/css')
@@ -7,4 +8,5 @@ mix.js('src/js/app.js', 'dist/js')
       enabled: true,
       useBuiltIns: "usage",
       targets: "firefox 50, IE 11"
-   });
+   })
+   .purgeCss({ enabled: true });
